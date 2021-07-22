ae21986318e011b59cccbb8d5fcdd0e0155a9d30
Jatumongkon <60840487+Jatumongkon@users.noreply.github.com>
2021-07-22 20:16:14 +0700

    final

---
+++ content/posts/a-post-with-a-cover.md
@@ -0,0 +1,14 @@
+---
+title: MyIntern
+date: 2021-07-22
+published: true
+tags: ['Intern', 'Project']
+series: false
+cover_image: ./images/mrtk.jpg
+canonical_url: false
+description: ""
+---
+
+![Image Alt](./images/1.jpg)
+![Image Alt](./images/Screenshot1.jpg)
+
---
+++ src/components/Author.vue
@@ -0,0 +1,66 @@
+<template>
+	<div class="author">
+
+		<g-image alt="Author image" class="author__image" src="~/assets/images/author.jpg" width="180" height="180" blur="5" />
+
+		<h1 v-if="showTitle" class="author__site-title">
+			{{ $static.metadata.siteName }}
+		</h1>
+
+		<div class="author__intro">
+				<p1>Computer Engineering </p1><p> Prince of Songkla University</p>
+		</div>
+
+		<p class="author__links">
+			<a href="//twitter.com/FirstJs2">Follow on Twitter</a>
+			<a href="//www.facebook.com/jatumongkon.sangpum">Facebook</a>
+			<a href="//github.com/Jatumongkon">GitHub</a>
+		</p>
+
+	</div>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName
+  }
+}
+</static-query>
+
+<script>
+export default {
+	props: ['showTitle']
+}
+</script>
+
+<style lang="scss">
+.author {
+	margin: 0 auto;
+	max-width: 500px;
+	text-align: center;
+	padding: calc(var(--space) / 2) 0;
+
+	&__image {
+		border-radius: 100%;
+		width: 90px;
+		height: 90px;
+		margin-bottom: 1em;
+	}
+
+	&__intro {
+		opacity: .8;
+	}
+
+	&__site-title {
+		font-size: 1.5em;
+	}
+
+	&__links {
+		margin-top: -.5em;
+		a {
+			margin: 0 .5em;
+		}
+	}
+}
+</style>
